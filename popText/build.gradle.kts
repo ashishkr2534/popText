76d7ed3774c206afa016c789e9541cecddffd704
Ashish Kumar <ashishkr2534@outlook.com>
2025-06-21 17:03:40 +0530

    Initial commit

---
+++ popText/build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    id("maven-publish")
+}
+
+android {
+    namespace = "com.ashish.poptext"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+//    composeOptions {
+//        kotlinCompilerExtensionVersion = "1.5.1"
+//    }
+}
+
+dependencies {
+
+//    implementation(libs.androidx.core.ktx)
+//    implementation(libs.androidx.appcompat)
+//    implementation(libs.material)
+//    testImplementation(libs.junit)
+//    androidTestImplementation(libs.androidx.junit)
+//    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.ui)
+
+    implementation(libs.androidx.material3)
+    implementation(platform(libs.androidx.compose.bom))
+
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            afterEvaluate {
+                from(components["release"])
+                groupId = "com.github.ashishkr2534"
+                artifactId = "popText"
+                version = "1.0.0"
+            }
+        }
+    }
+}
---
+++ popText/src/main/java/com/ashish/poptext/PopText.kt
@@ -0,0 +1,18 @@
+package com.ashish.poptext
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color.Companion.Green
+import androidx.compose.ui.graphics.Color.Companion.Red
+
+/**
+ * Created by Ashish Kr on 21,June,2025
+ */
+@Composable
+fun PopText(){
+    Text(
+        text = "Text Value",
+//        modifier = mo,
+        color = Red
+    )
+}
