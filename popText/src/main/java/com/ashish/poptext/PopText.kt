4202af6b0c63f41e99c324fee7826600b029308f
Ashish Kumar <ashishkr2534@outlook.com>
2025-06-24 00:20:25 +0530

    Green button v.1.0.2 for font new added pixel type

--- popText/src/main/java/com/ashish/poptext/PopText.kt
+++ popText/src/main/java/com/ashish/poptext/PopText.kt
@@ -1,12 +1,23 @@
 package com.ashish.poptext
 
 import android.widget.Toast
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.graphics.colorspace.WhitePoint
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 /**
  * Created by Ashish Kr on 21,June,2025
@@ -28,5 +39,36 @@ fun ButtonGreen(){
     }) {
         Text(text = "Green Button", color = Green)
 
+    }
+}
+
+@Composable
+fun PixelButtonPrimary(text: String,onClick: () -> Unit,modifier: Modifier){
+    Button(
+        onClick = onClick,
+        modifier  = modifier,
+//        modifier = Modifier.padding(top = 16.dp)
+//            .fillMaxWidth(0.8f)
+//            .height(46.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF00C853)),
+        shape = RoundedCornerShape(14.dp),
+        elevation = ButtonDefaults.buttonElevation(8.dp)
+    ) {
+        Text(text, fontFamily = PixelFamilyBold, fontSize = 18.sp, color = White)
+    }
+}
+
+@Composable
+fun PixelButtonDismiss(text: String, onClick: () -> Unit, modifier: Modifier){
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+//            .fillMaxWidth(0.8f)
+//            .height(46.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF949A96)),
+        shape = RoundedCornerShape(14.dp),
+        elevation = ButtonDefaults.buttonElevation(8.dp)
+    ) {
+        Text(text, fontFamily = PixelFamilyBold, fontSize = 18.sp,color = White)
     }
 }
---
+++ popText/src/main/java/com/ashish/poptext/Type.kt
@@ -0,0 +1,21 @@
+package com.ashish.poptext
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+
+/**
+ * Created by Ashish Kr on 24,June,2025
+ */
+val PixelFamilyBold = FontFamily(
+    Font(R.font.pixelifypans_bold, FontWeight.Bold)
+)
+val PixelFamilySemiBold = FontFamily(
+    Font(R.font.pixelifysans_semibold, FontWeight.SemiBold)
+)
+val PixelFamilyRegular = FontFamily(
+    Font(R.font.pixelifysans_regular, FontWeight.Normal)
+)
+val PixelFamilyMedium = FontFamily(
+    Font(R.font.pixelifysans_medium, FontWeight.Medium)
+)
