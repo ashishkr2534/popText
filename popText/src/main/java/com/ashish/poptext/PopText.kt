2ba6b71bb42118c5f57f0d02f6263fc51fa763d5
Ashish Kumar <ashishkr2534@outlook.com>
2025-06-23 23:14:07 +0530

    Green button v.02

--- popText/src/main/java/com/ashish/poptext/PopText.kt
+++ popText/src/main/java/com/ashish/poptext/PopText.kt
@@ -1,9 +1,12 @@
 package com.ashish.poptext
 
+import android.widget.Toast
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.platform.LocalContext
 
 /**
  * Created by Ashish Kr on 21,June,2025
@@ -15,4 +18,15 @@ fun PopText(){
 //        modifier = mo,
         color = Red
     )
+}
+
+@Composable
+fun ButtonGreen(){
+    val context = LocalContext.current
+    Button(onClick = {
+        Toast.makeText(context,"Button Clicked",Toast.LENGTH_SHORT).show()
+    }) {
+        Text(text = "Green Button", color = Green)
+
+    }
 }
